7bfadfe1ff5fab1fe8a428efdd145e1f9192af08
Romain Durand Saint Omer <romain.dso@gmail.com>
2017-01-01 19:21:16 +0100

    Better responsive design on starship details screen

--- src/components/screens/StarshipDetails.js
+++ src/components/screens/StarshipDetails.js
@@ -24,24 +24,24 @@ class StarshipDetails extends Component {
     return (
       <div className="StarshipDetails">
         <h1>{name}</h1>
-        <div className="StarshipDetails-main-info">
-          <h3>{model}</h3>
-        </div>
-        <div className="StarshipDetails-properties">
-          <h4>Main properties :</h4>
-          <ul>
-            <li>Length <span>{length} (meters)</span></li>
-            <li>Cargo capacity <span>{cargo_capacity} (kilograms)</span></li>
-            <li>Hyperdrive rating <span>{hyperdrive_rating}</span></li>
-            <li>Passengers <span>{passengers}</span></li>
-          </ul>
-          <p>Built with love by <b>{manufacturer}</b></p>
-        </div>
-        <div>
-          <h4>Pilots (unconfirmed)</h4>
-          <img src="http://facetheforce.today/random/150?r=1" alt="pilot" />
-          <img src="http://facetheforce.today/random/150?r=2" alt="pilot" />
-          <img src="http://facetheforce.today/random/150?r=3" alt="pilot" />
+        <h3>{model}</h3>
+        <div className="StarshipDetails-wrapper">
+          <div className="StarshipDetails-properties">
+            <h4>Main properties :</h4>
+            <ul>
+              <li>Length <span>{length} (meters)</span></li>
+              <li>Cargo capacity <span>{cargo_capacity} (kilograms)</span></li>
+              <li>Hyperdrive rating <span>{hyperdrive_rating}</span></li>
+              <li>Passengers <span>{passengers}</span></li>
+            </ul>
+            <p>Built with love by <b>{manufacturer}</b></p>
+          </div>
+          <div className="StarshipDetails-pilots">
+            <h4>Pilots (fake) :</h4>
+            <img src="http://facetheforce.today/random/150?r=1" alt="pilot" />
+            <img src="http://facetheforce.today/random/150?r=2" alt="pilot" />
+            <img src="http://facetheforce.today/random/150?r=3" alt="pilot" />
+          </div>
         </div>
       </div>
     );
