c3aec3397d72402e6eebc9d9eebf4664a5565501
romaindso <romain.dso@gmail.com>
2017-01-03 14:30:57 +0100

    Update prod files

--- public/sw.js
+++ public/sw.js
@@ -10,8 +10,8 @@ self.addEventListener('install', function(e){
         './favicon.ico',
         './bootstrap-theme.min.css',
         './bootstrap.min.css',
-        './static/js/main.242846c5.js',
-        './static/js/main.242846c5.js.map',
+        './static/js/main.654275d1.js',
+        './static/js/main.654275d1.js.map',
         './static/css/main.999d12a1.css',
         './static/css/main.999d12a1.css.map',
         './static/media/footer.f2d9fcde.jpg'
