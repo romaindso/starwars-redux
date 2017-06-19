2216ad771802bb24c3bbd769a7ed32bff87b2f9e
Romain Durand Saint Omer <romain.dso@gmail.com>
2017-06-19 21:39:15 +0200

    refactor: use lookup tables for characters reducer

--- src/reducers/characters.js
+++ src/reducers/characters.js
@@ -1,20 +1,52 @@
+import { combineReducers } from 'redux';
 import character from './character';
 
-export default (state=[], action) => {
-
+// Sub Reducers
+const byId = (state={}, action) => {
   switch (action.type) {
     case 'RECEIVE_CHARACTERS':
-      return [...state, ...action.data.map(item => {
-        let act = {data: {...item}, type: action.type}
-        return character(undefined, act);
-      })];
+      let newState = {...state};
+      action.data.map(item => {
+        let act = {data: {...item}, type: action.type};
+        let id = item.url.match(/\/([0-9]+)\/$/)[1];
+        newState[id] = character(undefined, act);
+      });
+      return newState;
 
     default:
-      return state
+      return state;
   }
-
 }
 
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case 'RECEIVE_CHARACTERS':
+      const charactersIdList = action.data.map(item => {
+        let id = item.url.match(/\/([0-9]+)\/$/)[1];
+        return id;
+      })
+      return [...state, ...charactersIdList];
+
+    default: 
+      return state;
+  }
+}
+
+// Main Reducer
+const characters = combineReducers({
+  byId,
+  allIds
+});
+
+export default characters;
+
+// Internal Selectors
+const getAllCharacters = (state) => {
+  return state.allIds.map(id => state.byId[id])
+};
+
+// Selectors
 export const getVisibleCharacters = (state, filter) => {
-  return filter === 'all' ? state : state.filter(character => character.gender === filter)
+  const allCharacters = getAllCharacters(state);
+  return filter === 'all' ? allCharacters : allCharacters.filter(character => character.gender === filter)
 }
